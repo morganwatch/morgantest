c0c60e5bf78a9f8b953f5239edcb506d8becb211
morganwatch <morganwatch@gmail.com>
2015-05-21 22:48:06 +0000

    initial commit

---
+++ morganTest2.R
@@ -0,0 +1,13 @@
+library(rworldmap)
+library(rgdal)
+library(RCurl)
+
+
+data(countryExData) 
+sPDF <- joinCountryData2Map( countryExData, joinCode = "ISO3", nameJoinColumn = "ISO3V10" )
+
+par(mai=c(0,0,0.2,0),xaxs="i",yaxs="i") 
+mapCountryData( sPDF, nameColumnToPlot="BIODIVERSITY"  )
+
+data <- mtcars
+
